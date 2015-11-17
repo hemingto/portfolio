83f0b472b32bccb6169e68c7130326f33277ca7b
Calvin Hemington <calvin.hemington@gmail.com>
2015-11-16 16:05:26 -0800

    add work descriptions, favicon, and other tweeks

--- jquery.js
+++ jquery.js
@@ -3,27 +3,15 @@ $(document).ready( function() {
 $("#left_brain_text").hide();
 
 $("#left_brain").click( function() {
-	$("#left_brain_text").fadeIn();
+	$("#left_brain_text").fadeIn("slow");
 	$("#right_brain_text").hide();
-	$("#")
 });
 
 $("#right_brain").click( function() {
-	$("#right_brain_text").fadeIn();
+	$("#right_brain_text").fadeIn("slow");
 	$("#left_brain_text").hide();
 
 });
-
-
-$("#download_button").hover(function() {
-	$("#download_text").toggleClass("font-weight");
-});
-
-var work_img_height = $(".work_img").css("height");
-
-console.log(work_img_height);
-
-$(".blue_overlay").css("height", work_img_height);
 
 });
 
